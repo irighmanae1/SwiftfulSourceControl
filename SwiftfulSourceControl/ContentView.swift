fc75ea36f557ae1acbc5908aff3680960a794b25
irishmanae1 <irishmanae123@icloud.com>
2025-05-26 18:54:52 -0400

    commit message

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -50,7 +50,7 @@ struct ContentView: View {
                             .foregroundStyle(.tint)
                         Text("Swiftful!!!")
 
-                        Button("Subscribe now") {
+                        Button("Subscribe") {
                             
                         }
                     }
