d17ed71698aeae789cf986e85395a1be7955c922
irishmanae1 <irishmanae123@icloud.com>
2025-05-26 18:55:14 -0400

    crap commit 2

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -48,7 +48,7 @@ struct ContentView: View {
                         Image(systemName: "globe")
                             .font(.largeTitle)
                             .foregroundStyle(.tint)
-                        Text("Swiftful!!!")
+                        Text("Swiftful Thinking!!!")
 
                         Button("Subscribe") {
                             
