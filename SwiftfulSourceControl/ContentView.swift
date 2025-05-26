89c826f3801711c4ac2e11a682a95042f09a9263
irishmanae1 <irishmanae123@icloud.com>
2025-05-26 08:45:08 -0400

    [Feature] Click me to subscribe now

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -46,7 +46,7 @@ struct ContentView: View {
                             .foregroundStyle(.tint)
                         Text("Swiftful thinking !!!!!!")
 
-                        Button("Click me") {
+                        Button("Subscribe now") {
                             
                         }
                     }
