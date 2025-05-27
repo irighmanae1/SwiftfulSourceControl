fd8750ee6bc1edfae7a5fec908a36fa1a11c161f
irishmanae1 <irishmanae123@icloud.com>
2025-05-27 10:00:49 -0400

    [Feature] Add ProfileView

---
+++ SwiftfulSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Liam Mulligan on 5/27/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("NAME")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
